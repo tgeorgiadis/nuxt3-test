452b01d44e35110c59c318cced7ee04ad7d1f9d8
Thomas Georgiadis <tgeorg94@gmail.com>
2022-04-26 23:06:51 +1000

    Add Capacitor and Android

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+   ssr: false,
 
+   // Target (https://go.nuxtjs.dev/config-target)
+   target: 'static',
 })
