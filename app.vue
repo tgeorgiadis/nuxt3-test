2583919beacc52ac24455fd5bfe04d9a5763aed8
Thomas Georgiadis <tgeorg94@gmail.com>
2022-05-09 21:34:48 +1000

    Add eslint

--- app.vue
+++ app.vue
@@ -1,30 +1,34 @@
-<script>
+<script lang="ts">
 export default {
-  data() {
+  data () {
     return {
       clientClicks: 0,
       globalClicks: 0
-    }
+    };
   },
   methods: {
-    async postClick() {
-      this.clientClicks++
-      const clickData = await $fetch('/api/click', {
-        method: 'POST',
+    async postClick () {
+      this.clientClicks++;
+      const clickData = await $fetch("/api/click", {
+        method: "POST"
         // body: data
-      })
-      this.globalClicks = clickData.globalClicks
+      });
+      this.globalClicks = clickData.globalClicks;
     }
   }
-}
+};
 </script>
 
 <template>
   <div class="grid place-items-center h-screen bg-emerald-100">
-    <div class="w-52 h-52 bg-green-800 rounded-full" @click="postClick"></div>
+    <div class="w-52 h-52 bg-green-800 rounded-full" @click="postClick" />
     <div class="text-center">
-      <p class="font-sans text-6xl">Your taps {{ clientClicks }}</p>
-      <p class="font-sans text-6xl">Global taps {{ globalClicks }}</p>
+      <p class="font-sans text-6xl">
+        Your taps {{ clientClicks }}
+      </p>
+      <p class="font-sans text-6xl">
+        Global taps {{ globalClicks }}
+      </p>
     </div>
   </div>
 </template>
--- server/api/click.post.ts
+++ server/api/click.post.ts
@@ -1,16 +1,17 @@
-import pkg from '@prisma/client';
+// eslint-disable-next-line import/default
+import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default async () => {
   await prisma.click.create({
     data: {}
-  })
+  });
 
-  const globalClicks = await prisma.click.count()
-  
+  const globalClicks = await prisma.click.count();
+
   return {
     globalClicks
-  }
-}
+  };
+};
